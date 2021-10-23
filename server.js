e3332f786c0b04218216933dc58a9af44426ab8d
giftilicious <90150892+giftilicious@users.noreply.github.com>
2021-10-23 19:20:11 -0400

    Updated server file with fuctions to read/write data

--- server.js
+++ server.js
@@ -14,29 +14,79 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+// Get route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// Get route for notes page
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-// GET request for notes
-app.get('/api/notes', (req, res) => {
-  // Send a message to the client
-  res.json(`${req.method} request received to get notes`);
+const readFromFile = util.promisify(fs.readFile);
+/**
+ *  Function to write data to the JSON file given a destination and some content
+ *  @param {string} destination The file you want to write to.
+ *  @param {object} content The content you want to write to the file.
+ *  @returns {void} Nothing
+ */
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+/**
+ *  Function to read data from a given a file and append some content
+ *  @param {object} content The content you want to append to the file.
+ *  @param {string} file The path to the file you want to save to.
+ *  @returns {void} Nothing
+ */
+const readAndAppend = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
 
-  // Log our request to the terminal
-  console.info(`${req.method} request received to get notes`);
+
+// API GET request for notes
+app.get('/api/notes', (req, res) => {
+    // Send a message to the client
+    res.json(`${req.method} request received to get notes`);
+    // Log our request to the terminal
+    console.info(`${req.method} request received to get notes`);
 });
 
-// POST request to add a note
+// API POST request to add a note
 app.post('/api/notes', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a note`);
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+
+    const { noteTitle, noteText } = req.body;
+
+    if (req.body) {
+        const newNote = {
+            noteTitle,
+            noteText,
+            tip_id: uuid(),
+        };
+
+        readAndAppend(newNote, './db.json');
+        res.json(`Note added successfully!`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+
+
 
 
 app.listen(PORT, () =>
-  console.log(`App listening on http://localhost:${PORT}!`)
+    console.log(`App listening on http://localhost:${PORT}!`)
 );
